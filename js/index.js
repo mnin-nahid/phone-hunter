1836f1df7e5a085c84f3a0edd002ecb1f48443ec
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-03-01 14:38:45 +0600

    js start

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('its working');
+const lodeData = inputValue => {
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(res => res.json())
+    .then(data => displayData(data.data))
+}
+lodeData();
+const displayData = phones => {
+    const phoneDetails = document.getElementById('phone-details');
+    for(const phone of phones){
+        console.log(phone.phone_name);
+    }
+}
